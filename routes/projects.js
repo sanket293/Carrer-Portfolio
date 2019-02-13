5abade902c5f29e47bc652fb973b70bf6c937a3f
Sanket <scvagadiya@gmail.com>
2019-02-12 20:05:06 -0500

    Added portfolio and Contact views

--- routes/projects.js
+++ routes/projects.js
@@ -6,6 +6,19 @@ router.get("/", (req, res, next) => {
   res.render("projects/index", { title: "Projects" }); 
 });
 
+router.get("/success-quotes", (req, res, next) => {
+  res.render("projects/successquotes", { title: "Success Quotes" }); 
+});
+
+
+router.get("/womb-to-world", (req, res, next) => {
+  res.render("projects/wombtoworld", { title: "Womb to World" }); 
+});
+
+
+router.get("/sudoku", (req, res, next) => {
+  res.render("projects/sudoku", { title: "Sudoku" }); 
+});
 
 
 module.exports = router;
